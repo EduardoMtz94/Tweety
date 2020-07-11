388a652a8a8ebc4d076c3e3662d8786e72662054
Eduardo Martinez <eduardo.martinez.sanchez@hotmail.com>
2020-07-10 19:56:30 -0500

    Make the timeline dynamic

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,13 +1,13 @@
 <div class="flex p-4 border-b border-b-gray-400">
     <div class="mr-2 flex-shrink-0">
-        <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+        <img src="{{ $tweet->user->avatar }}" alt="" class="rounded-full mr-2">
     </div>
 
     <div>
-        <h5 class="font-bold mb-4">John Doe</h5>
+        <h5 class="font-bold mb-4">{{ $tweet->user->name }}</h5>
 
         <p class="text-sm">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur vel, repellendus laudantium modi alias cumque quia aliquam beatae quibusdam, ex quidem dolorem. Officiis modi excepturi, qui repellat laboriosam placeat incidunt!
+            {{ $tweet->body }}
         </p>
     </div>
 </div>
