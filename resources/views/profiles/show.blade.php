3c6629cec8d712fd004481722912e994f22e0c30
Eduardo Martinez <eduardo.martinez.sanchez@hotmail.com>
2020-07-14 17:29:23 -0500

    Build the follow form

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,20 +1,26 @@
-@extends('layouts.app')
-
-@section('content')
-    
+<x-app>
     <header class="mb-6 relative">
-        <img src="/images/default-profile-banner.jpg" alt="" class="mb-2">
+        <div class="relative">
+            <img src="/images/default-profile-banner.jpg" alt="" class="mb-2">
+    
+            <img src="{{ $user->avatar }}" alt="" 
+            class="rounded-full mr-2 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2"
+            style="left:50%;" 
+            width="150">
+        </div>
 
-        <div class="flex justify-between items-center mb-4">
+        <div class="flex justify-between items-center mb-6">
             
             <div>
                 <h2 class="font-bold text-2xl mb-0">{{ $user->name }}</h2>
                 <p class="text-sm">Joined {{ $user->created_at->diffForHumans() }}</p>
             </div>
 
-            <div>
+            <div class="flex">
                 <a href="" class="rounded-full border border-gray-300 py-2 px-2 text-black text-xs mr-2">Edit Profile</a>
-                <a href="" class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">Follow me</a>
+                
+                <x-follow-button :user="$user"></x-follow-button>
+
             </div>
 
         </div>
@@ -23,14 +29,10 @@
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, fuga eligendi et vero excepturi ab quos sint non recusandae quasi cum unde sed iusto sequi provident aperiam facere voluptatum beatae!
         </p>
 
-        <img src="{{ $user->avatar }}" alt="" 
-        class="rounded-full mr-2 absolute" 
-        style="width: 150px; left: calc(50% - 75px); top: 138px">
-
     </header>
 
     @include('_timeline', [
         'tweets' => $user->tweets
     ])
     
-@endsection
+</x-app>
