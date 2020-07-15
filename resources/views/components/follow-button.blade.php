ded3d0e74002e4b73b25ef2510698e16cdcb1004
Eduardo Martinez <eduardo.martinez.sanchez@hotmail.com>
2020-07-14 21:23:09 -0500

    Profile Authorization Logic

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,9 +1,11 @@
-<form action="/profiles/{{ $user->name }}/follow" method="POST">
-    @csrf
+@unless (current_user()->is($user))
+    <form action="/profiles/{{ $user->name }}/follow" method="POST">
+        @csrf
 
-    <button type="submit" 
-            class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs"
-    >
-        {{ auth()->user()->following($user) ? 'Unfollow me' : 'Follow me' }}
-    </button>
-</form>
+        <button type="submit" 
+                class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs"
+        >
+            {{ current_user()->following($user) ? 'Unfollow me' : 'Follow me' }}
+        </button>
+    </form>
+@endunless
