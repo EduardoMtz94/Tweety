368b86681282bb2b923231a558fd1d74f08fa980
Eduardo Martinez <eduardo.martinez.sanchez@hotmail.com>
2020-07-17 02:12:19 -0500

    Clean up

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -2,15 +2,15 @@
     <form method="POST" action="/tweets">
         @csrf
 
-        <textarea name="body" class="w-full" placeholder="What's up doc?"></textarea>
+        <textarea name="body" class="w-full" placeholder="What's up doc?" required></textarea>
         
         <hr class="my-4">
         
-        <footer class="flex justify-between">
+        <footer class="flex justify-between items-center">
             <img src="{{ auth()->user()->avatar }}" alt="your avatar" class="rounded-full mr-2" width="50" height="50">
             
-            <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-4 text-white">
-                Tweet-a-roo!
+            <button type="submit" class="bg-blue-500 hover:bg-blue-600 rounded-lg shadow px-10 text-sm text-white h-10">
+                Publish
             </button>
         </footer>
         
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -11,7 +11,7 @@
 
         <div class="flex justify-between items-center mb-6">
             
-            <div>
+            <div style="max-width: 270px">
                 <h2 class="font-bold text-2xl mb-0">{{ $user->name }}</h2>
                 <p class="text-sm">Joined {{ $user->created_at->diffForHumans() }}</p>
             </div>
@@ -38,7 +38,7 @@
     </header>
 
     @include('_timeline', [
-        'tweets' => $user->tweets
+        'tweets' => $tweets
     ])
     
 </x-app>
